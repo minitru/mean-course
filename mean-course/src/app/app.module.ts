b6322a6ffccf0fa094b397aa6e8e3da363f33415
sean <sean@keno-2.local>
2018-10-12 03:37:19 -0400

    UI STUFF

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -5,16 +5,23 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule, MatCardModule, MatButtonModule, MatToolbar, MatToolbarModule } from '@angular/material';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent {
-  newPost='NO CONTENT';
-  enteredValue='';
+  newPost = 'NO CONTENT';
+  enteredValue = '';
 
-  onAddPost(){
-    this.newPost=this.enteredValue;
+  onAddPost() {
+    this.newPost = this.enteredValue;
     alert(this.newPost);
   }
 }
