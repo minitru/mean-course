8dd710abb545d3200a45bf49d03d73792167e47f
sean <sean@keno-2.local>
2018-10-19 05:20:56 -0400

    create post stub

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,10 +1,27 @@
 const express = require('express');
 
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE,OPTIONS');
+  next();
+});
 
-app.use('/api/posts',(req, res, next) => {
+app.post('/api/posts',(req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'posts entered ok'
+  });
+});
+
+app.get('/api/posts',(req, res, next) => {
   const posts = [
     { id: 'a123', title: 'First title', content: 'first content'},
     { id: 'a222', title: 'Second title', content: 'Second content'},
