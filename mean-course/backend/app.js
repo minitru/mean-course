ea04a8b5b50b74b5f6b568ab9df38d5c586b939a
sean <sean@keno-2.local>
2018-10-20 03:52:23 -0400

    Added mongo

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect("mongodb://dev.maclawran.ca/node-angular?retryWrites=true")
+  .then(() => {
+    console.log('Connected to mongo OK');
+  })
+  .catch(() => {
+    console.log('Mongo error!');
+  });
+
 const bodyParser = require('body-parser');
+
+const Post = require('./models/post');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -13,23 +25,26 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts',(req, res, next) => {
-  const post = req.body;
-  console.log(post);
+app.post('/api/posts', (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  post.save();
   res.status(201).json({
     message: 'posts entered ok'
   });
 });
 
 app.get('/api/posts',(req, res, next) => {
-  const posts = [
-    { id: 'a123', title: 'First title', content: 'first content'},
-    { id: 'a222', title: 'Second title', content: 'Second content'},
-  ];
-  res.status(200).json({
-    message: 'posts fetched ok',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'posts fetched ok',
+      posts: documents
+    });
   });
+
 });
 
 module.exports = app;
