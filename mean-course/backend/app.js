02468d119ad229711dc32bfe7dfb5854c039f9f5
sean <sean@keno-2.local>
2018-11-11 04:20:56 -0500

    Sync with course

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -1,40 +1,34 @@
-const path = require("path");
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
 
-const postsRoutes = require("./routes/posts");
+const postsRoutes = require('./routes/posts');
 
 const app = express();
 
-mongoose
-  .connect(
-    "mongodb+srv://max:QuBqs0T45GDKPlIG@cluster0-ntrwp.mongodb.net/node-angular?retryWrites=true"
-  )
+mongoose.connect("mongodb://dev.maclawran.ca/node-angular?retryWrites=true")
   .then(() => {
-    console.log("Connected to database!");
+    console.log('Connected to mongo OK');
   })
   .catch(() => {
-    console.log("Connection failed!");
+    console.log('Mongo error!');
   });
 
+const bodyParser = require('body-parser');
+
+const Post = require('./models/post');
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/images", express.static(path.join("backend/images")));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,PATCH,DELETE,OPTIONS');
   next();
 });
 
-app.use("/api/posts", postsRoutes);
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
