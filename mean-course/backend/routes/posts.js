4687f92985698ae872d15ab75eaae934df4545a6
sean <sean@keno-2.local>
2018-11-13 04:15:28 -0500

    handle post numbering

--- mean-course/backend/routes/posts.js
+++ mean-course/backend/routes/posts.js
@@ -78,15 +78,20 @@ router.get("", (req, res, next) => {
   const pageSize = +req.query.pagesize;   // + CONVERTS STR->NUM
   const currentPage = +req.query.page;
   const postQuery = Post.find();
+  let fetchedPosts;
   if (pageSize && currentPage) {
     postQuery
     .skip(pageSize * (currentPage - 1))
     .limit(pageSize);
   }
   postQuery.then(documents => {
+    fetchedPosts = documents;
+    return Post.count();
+  }).then( count => {
     res.status(200).json({
       message: "Posts fetched successfully!",
-      posts: documents
+      posts: fetchedPosts,
+      maxPosts: count
     });
   });
 });
