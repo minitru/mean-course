a437387f35d6ca82ede0b8d939e7dbc0afd5670d
sean <sean@keno-2.local>
2018-11-24 06:15:46 -0500

    authorization stuff

--- mean-course/src/app/auth/auth.service.ts
+++ mean-course/src/app/auth/auth.service.ts
@@ -2,10 +2,22 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { AuthData } from './auth-data.model';
+import { Subject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  constructor (private http: HttpClient){}
+  private token: string;
+  private authStatusListener = new Subject<boolean>();
+
+  constructor (private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
+
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
 
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
@@ -17,9 +29,11 @@ export class AuthService {
 
   login(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
-    this.http.post('http://localhost:3000/api/user/login', authData)
-      .subscribe (response => {
-        console.log(response);
-      })
+    this.http.post<{token: string}>('http://localhost:3000/api/user/login', authData)
+      .subscribe(response => {
+        const token = response.token;
+        this.token = token;
+        this.authStatusListener.next(true);
+      });
   }
 }
