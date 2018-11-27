9455051e5d124476f7d8bb3d233e2b218f28352b
sean <sean@keno-2.local>
2018-11-27 04:31:28 -0500

    added error component replaced alert

--- mean-course/src/app/error-interceptor.ts
+++ mean-course/src/app/error-interceptor.ts
@@ -7,16 +7,22 @@ import {
 
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { ErrorComponent } from './error/error.component';
 
+@Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  intercept(req: HttpRequest<any>, next: HttpHandler ) {
+  constructor(private dialog: MatDialog ){}
 
+  intercept(req: HttpRequest<any>, next: HttpHandler ) {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         console.log(error);
         // MONGOOSE ADDS ANOTHER LEVEL OF ERROR FOR THIS INSANITY
-        alert(error.error.error.message);
+        // alert(error.error.error.message);
+        this.dialog.open(ErrorComponent);
         return throwError(error);
       })
     );
